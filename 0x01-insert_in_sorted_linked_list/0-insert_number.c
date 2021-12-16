634244f09213f725200fc395f989e3b3cfccf7de
samilo-del <camilovalenciapadilla@gmail.com>
2021-12-16 04:32:03 +0000

    add betty in point 0

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,38 +3,38 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
+ * insert_node - adds a new node at the end of a listint_t list
  * @head: pointer to pointer of first node of listint_t list
  * @number: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
-    listint_t *previous = NULL;
+listint_t *new;
+listint_t *current;
+listint_t *previous = NULL;
 
-    current = *head;
+current = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+new->n = number;
+new->next = NULL;
 
-    if (*head == NULL || (*head)->n >= number)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
-    while (current && number >= current->n)
-    {
-        previous = current;
-        current = current->next;
-    }
-    new->next = current;
-    previous->next = new;
-    return (new);
+if (*head == NULL || (*head)->n >= number)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+while (current && number >= current->n)
+{
+previous = current;
+current = current->next;
+}
+new->next = current;
+previous->next = new;
+return (new);
 }
