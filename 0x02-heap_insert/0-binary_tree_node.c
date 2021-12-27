dcc5e53832b26efef4618caed3714e4e1f00b543
samilo-del <camilovalenciapadilla@gmail.com>
2021-12-27 01:34:49 +0000

    add Betty coding style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,11 +5,11 @@
 
 /**
  * binary_tree_node - function that creates a binary tree node
- * 
  * @parent:  is a pointer to the parent node of the node to create.
  * @value:  is the value to put in the new node.
+ *
+ * Return: new
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
