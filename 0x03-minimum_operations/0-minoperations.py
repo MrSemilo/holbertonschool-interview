a14f0970b36cb3112d16b6b8d8c0288e6fa63cae
samilo-del <camilovalenciapadilla@gmail.com>
2022-01-09 16:57:26 +0000

    ADD POINT

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Operations"""
+
+
+def minOperations(n):
+    """ function"""
+    if not isinstance(n, int) or n < 2:
+        return 0
+    operation = 0
+    i = 3
+
+    while n % 2 == 0:
+        operation += 2
+        n//=2
+    while i <= n:
+        while n % i == 0:
+            operation += i
+            n = n//i
+        i += 2
+    return int(operation)
