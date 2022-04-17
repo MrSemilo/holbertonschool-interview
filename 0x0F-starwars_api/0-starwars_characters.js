d98883c360668bbeafa9f66ab71c81bf15717f07
samilo-del <camilovalenciapadilla@gmail.com>
2022-04-17 17:17:15 +0000

    API starwars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,24 +3,24 @@
  * Prints all characters of specified Star Wars movie
  * First argument is Movie ID
  */
- const request = require('request');
+const request = require('request');
 
- function pchars (chars, i) {
-   if (i === chars.length) return;
-   request(chars[i], function (err, resp, body) {
-     if (err) console.log(err);
-     else {
-       console.log(JSON.parse(body).name);
-       pchars(chars, ++i);
-     }
-   });
- }
- 
- request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
-   function (err, resp, body) {
-     if (err) console.log(err);
-     else {
-       const chars = JSON.parse(body).characters;
-       pchars(chars, 0);
-     }
-   });
+function pchars (chars, i) {
+  if (i === chars.length) return;
+  request(chars[i], function (err, resp, body) {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.parse(body).name);
+      pchars(chars, ++i);
+    }
+  });
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
+  function (err, resp, body) {
+    if (err) console.log(err);
+    else {
+      const chars = JSON.parse(body).characters;
+      pchars(chars, 0);
+    }
+  });
