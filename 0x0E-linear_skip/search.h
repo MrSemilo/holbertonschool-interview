e261c9001bcbb4d4fb1fcc0bb70ea8c301e85460
samilo-del <camilovalenciapadilla@gmail.com>
2022-04-17 16:51:20 +0000

    add n

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -1,8 +1,10 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -15,15 +17,20 @@
  * Description: singly linked list node structure with an express lane
  * for Holberton project
  */
-typedef struct skiplist_s
+struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+};
+typedef struct skiplist_s skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
 skiplist_t *check_lin(skiplist_t *list, int value, skiplist_t *stop);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
 
 #endif /* SEARCH_H */
